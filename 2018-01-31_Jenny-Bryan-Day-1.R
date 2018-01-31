c6800836af166f9c7e1671cba8e35bdcdfbec3e9
Kurtis Pivert <kurtis.pivert@gmail.com>
2018-01-31 09:05:52 -0800

    Day 1 Morning Pre-Conf

--- 2018-01-31_Jenny-Bryan-Day-1.R
+++ 2018-01-31_Jenny-Bryan-Day-1.R
@@ -2,3 +2,16 @@
 # Jenny Bryan: Day 1 ------------------------------------------------------
 
 
+
+# 1. Load Packages -------------------------------------------------------
+
+  require(tidyverse)
+  require(extrafont)
+  loadfonts(quiet = TRUE)
+  require(usethis)
+  
+
+# 2. Load Challenges ------------------------------------------------------
+
+  usethis::use_course("rstd.io/forgot_1")  
+  usethis::use_course("rstd.io/forgot_2")
